3c512b86abd2a54dde5b501af204c7fbe370de88
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-16 22:46:27 -0500

    added additional ampersand

--- projects/random-number-generator/js/random.js
+++ projects/random-number-generator/js/random.js
@@ -5,7 +5,7 @@ num1 = +prompt("Please provide your highest number")
 num2 = +prompt("Please provide your lowest number")
 
 
-if (num1, num2) {
+if (num1 && num2) {
     // Collect input from a user
     // Use Math.random() and the user's number to generate a random number
     const randomNumber = Math.floor(Math.random() * (num1 - num2 + 1)) + num2;
