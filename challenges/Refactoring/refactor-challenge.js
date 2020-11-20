94071489a412d2701ebe706947e34e67786013f8
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-20 12:37:03 -0500

    code has been refactored, now 15 lines of code

--- challenges/Refactoring/refactor-challenge.js
+++ challenges/Refactoring/refactor-challenge.js
@@ -4,65 +4,12 @@ let green;
 let blue;
 let randomRGB;
 
-for (let i = 1; i <= 10; i++)
+for (let i = 1; i <= 10; i++) {
     red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">1</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">2</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">3</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">4</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">5</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">6</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">7</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">8</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">9</div>`;
-
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb(${red}, ${green}, ${blue})`;
-html += `<div style="background-color: ${randomRGB}">10</div>`;
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    randomRGB = `rgb(${red}, ${green}, ${blue})`;
+    html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+}
 
 document.querySelector('main').innerHTML = html;
