61b0add8457f72abd61cdcc1f4bc49ade4b0f694
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-21 09:29:05 -0500

    guessing game updated to a for loop

--- challenges/Guessing-game/app.js
+++ challenges/Guessing-game/app.js
@@ -1,25 +1,21 @@
 const main = document.querySelector('main');
 const randomNumber = getRandomNumber(10);
-let guess;
-let message;
-let trys = 0;
+// let message;
+
 
 function getRandomNumber(upper) {
     return Math.floor(Math.random() * upper) + 1;
 }
 
 
-do {
-    guess = prompt("I'm thinking of a number between 1 and 10. Guess what it is");
-    trys++
+for (let trys = 0; trys < 10; trys++) {
+    let guess = prompt("I'm thinking of a number between 1 and 10. Guess what it is");
     if (+guess == randomNumber) {
-        message = `<h1>Congrats! You guessed the right number! It was ${randomNumber} and it took you ${trys} amount of try(s)!</h1>`
+        var message = `<h1>Congrats! You guessed the right number! It was ${randomNumber} and it took you ${trys} amount of try(s)!</h1>`
         break
     } else {
         message = `You did not guess the number it was ${randomNumber}`
     }
-} while (trys < 10);
+}
 
-document.write(`<h1>${message}</h1>`)
-
-
+main.innerHTML = `<h1>${message}</h1>`
