2211ffd5132117a9d5740dd52023d5a3e57ac0d4
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-21 14:21:38 -0500

    added multidimensional arrays

---
+++ practice/js-loop-though-arrays/js/multidimensional-arr.js
@@ -0,0 +1,24 @@
+const playlist = [
+    ['So What', 'Miles Davis', '9:04'],
+    ['Respect', 'Aretha Franklin', '2:45'],
+    ['What a Wonderful World', 'Louis Armstrong', '2:21'],
+    ['At Last', 'Ella Fitzgerald', '4:18'],
+    ['Three Little Birds', 'Bob Marley and the Wailers', '3:01'],
+    ['The Way You Look Tonight', 'Frank Sinatra', '3:21']
+];
+
+//const myArtist = `${playlist[0][1]}, ${playlist[1][1]}, ${playlist[2][1]}`;
+
+const createListItems = (arr) => {
+    let items = '';
+    for (let i = 0; i < arr.length; i++) {
+        items += `<li>${arr[i][0]}, by ${arr[i][1]} - ${arr[i][2]}</li>`;
+    }
+    return items;
+}
+
+document.querySelector('main').innerHTML = `
+    <ol>
+      ${createListItems(playlist)}
+    </ol>
+  `;
