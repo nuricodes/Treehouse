f913809d5fbdb18b04eb481d8a990b68ad15c083
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-23 19:19:45 -0500

    updated remove buttons

--- practice/DOM-manipulation/app.js
+++ practice/DOM-manipulation/app.js
@@ -3,19 +3,20 @@ const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description'); //p with the class description
 const descriptionButton = document.querySelector('button.description');
+const listUL = document.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton = document.querySelector('button.removeItemButton');
+// const removeItemButton = document.querySelector('button.removeItemButton');
 
-listDiv.addEventListener('mouseover', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toUpperCase();
-  }
-})
 
-listDiv.addEventListener('mouseout', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toLowerCase();
+
+listUL.addEventListener('click', (event) => {
+  if (event.target.tagName == 'BUTTON') {
+    // would be li since button is nested in li
+    let li = event.target.parentNode;
+    let ul = li.parentNode;
+    ul.removeChild(li);
+
   }
 })
 
@@ -46,12 +47,12 @@ addItemButton.addEventListener('click', () => {
   addItemInput.value = '';
 })
 
-removeItemButton.addEventListener('click', () => {
-  //store element you want to append to in variable so we can access later
-  let ul = document.getElementsByTagName('ul')[0];
-  // selects the last li
-  let li = document.querySelector('li:last-child');
-  // selects the last li
-  ul.removeChild(li);
+// removeItemButton.addEventListener('click', () => {
+//   //store element you want to append to in variable so we can access later
+//   let ul = document.getElementsByTagName('ul')[0];
+//   // selects the last li
+//   let li = document.querySelector('li:last-child');
+//   // selects the last li
+//   ul.removeChild(li);
 
-})                               
+// })                               
