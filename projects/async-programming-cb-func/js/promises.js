04bc9a414aa7426553047cb0d50459abb83f7615
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-25 11:13:20 -0500

    array of promises are logging to the console

--- projects/async-programming-cb-func/js/promises.js
+++ projects/async-programming-cb-func/js/promises.js
@@ -3,22 +3,32 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        //changes the status of the promise from pending to fulfilled
+        resolve(data);
+      } else {
+        //if status isn't 200 call reject
+        //Error will display stack traits and status text for a more meaningful error
+        reject(Error(xhr.statusText))
+      }
+    };
+    //incase of connectivity error reject promise
+    xhr.onerror = () => reject(Error('A network error occured'));
+    xhr.send();
+  });
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+  const profiles = json.people.map(person => {
+    return getJSON(wikiUrl + person.name);
+  });
+  return profiles;
 }
 
 function generateHTML(data) {
@@ -33,6 +43,10 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
+  getJSON(astrosUrl)
+    .then(getProfiles)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
   event.target.remove();
 });
