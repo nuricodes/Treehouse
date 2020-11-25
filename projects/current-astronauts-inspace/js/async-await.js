47db04d9905e3c6d6953cc409c4579be389aaf3e
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-25 16:33:41 -0500

    async

--- projects/current-astronauts-inspace/js/async-await.js
+++ projects/current-astronauts-inspace/js/async-await.js
@@ -4,12 +4,27 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+
+async function getJSON(url) {
+  try {
+    // add data fetching statement
+    //the value awaits the promise returned by fetch
+    const response = await fetch(url);
+    // the function is going to wait for response.json to resolve or reject
+    return await response.json();
+    //when an error occurs catch gets the details
+  } catch (error) {
+    // the error here will bubble up and be caught by try catch blocks at the top level
+    throw error;
+
+  }
+
+}
+
 //this function will return the array of promised object captured in the variable profiles
 async function getPeopleInSpace(url) {
-  // await keyword will wait for resolved promise returned by fetch and get the fulfillment value out and assign it 
-  const peopleResponse = await fetch(url);
   //read the response and await the json
-  const peopleJSON = await peopleResponse.json();
+  const peopleJSON = await getJSON(url);
 
   //map of the array of objects in the peopleJSON and fetch data from wiki api
   //iterate over the people property in the peopleJSON
@@ -18,18 +33,17 @@ async function getPeopleInSpace(url) {
   const profiles = peopleJSON.people.map(async (person) => {
     //to prepare to pull from first wiki api
     const craft = person.craft;
-    //awaits the response object of fetch then concatname on each iteration
-    const profileResponse = await fetch(wikiUrl + person.name)
+
     //await a result promise from profile response
-    const profileJSON = await profileResponse.json();
+    const profileJSON = await getJSON(wikiURL + person.name);
 
     // here we return object all the properties from profile json and craft
     return { ...profileJSON, craft };
   });
   //will wait till the aggregrated promise to be resolved in to a single promise before it returns the data
-  return Promise.all(profiles)
+  return Promise.all(profiles);
 }
-console.log(getPeopleInSpace(astrosUrl))
+
 
 // Generate the markup for each profile
 function generateHTML(data) {
@@ -50,6 +64,10 @@ btn.addEventListener('click', (event) => {
 
   getPeopleInSpace(astrosUrl)
     .then(generateHTML)
+    .catch(e => {
+      peopleList.innerHTML = '<h3>Something went wrong!</h3>'
+      console.error(e);
+    })
     .finally(() => event.target.remove())
 
 });
