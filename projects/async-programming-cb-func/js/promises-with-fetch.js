36bfa536e1f368f252de595a073cfaf35861ff58
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-25 11:49:02 -0500

    added fetch in lieu of get JSON in the event listener and deleted entire getJSON function

--- projects/async-programming-cb-func/js/promises-with-fetch.js
+++ projects/async-programming-cb-func/js/promises-with-fetch.js
@@ -3,27 +3,6 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                //changes the status of the promise from pending to fulfilled
-                resolve(data);
-            } else {
-                //if status isn't 200 call reject
-                //Error will display stack traits and status text for a more meaningful error
-                reject(Error(xhr.statusText))
-            }
-        };
-        //incase of connectivity error reject promise
-        xhr.onerror = () => reject(Error('A network error occured'));
-        xhr.send();
-    });
-}
-
 function getProfiles(json) {
     const profiles = json.people.map(person => {
         return getJSON(wikiUrl + person.name);
@@ -46,7 +25,7 @@ function generateHTML(data) {
 
 btn.addEventListener('click', (event) => {
     event.target.textContent = 'Loading';
-    getJSON(astrosUrl)
+    fetch(astrosUrl)
         .then(getProfiles)
         .then(generateHTML)
         .catch(err => {
