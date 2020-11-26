4bbeaaaca5eafb654d6966395410d29e16978fda
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-25 20:56:29 -0500

    random dog showing to page with each refresh

--- projects/dogs-api/js/app.js
+++ projects/dogs-api/js/app.js
@@ -1,21 +1,29 @@
 const select = document.getElementById('breeds');
-const card = document.querySelector('.card'); 
+const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+
+
 fetch('https://dog.ceo/api/breeds/image/random')
   //in order to use the returned data we need to parse
   .then(response => response.json())
-  .then(data => console.log(data.message))
+  .then(data => generateImage(data.message))
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-
+function generateImage(data) {
+  const html = `
+  <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+  `;
+  card.innerHTML = html;
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
