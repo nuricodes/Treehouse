0d7355702d3738aac9efeb68d99ab822c15b8a46
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-26 16:02:43 -0500

    add books and add patron methods added to the library class

--- projects/library/Library.js
+++ projects/library/Library.js
@@ -1,7 +1,15 @@
 class Library {
-   constructor(books, patrons){
-    this.books = [];
-     this.patrons = [];
+   constructor(books, patrons) {
+      this.books = [];
+      this.patrons = [];
    }
-  
+
+   addBook(book) {
+      this.books.push(book)
+   }
+
+   addPatron(patron) {
+      this.patrons.push(patron)
+   }
+
 }
