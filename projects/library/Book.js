01a1ea5bb0d81953408dcf1db6ad068e475f88df
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-26 20:02:10 -0500

    checkout and return methods for patron updated

--- projects/library/Book.js
+++ projects/library/Book.js
@@ -1,8 +1,11 @@
 class Book {
-  constructor(title, author, ISBN){
+  constructor(title, author, ISBN, out, dueDate, patron) {
     this.title = title;
     this.author = author;
     this.ISBN = ISBN;
-  
+    this.out = false;
+    this.dueDate = null;
+    this.patron = null;
+
   }
 }
--- projects/library/Patron.js
+++ projects/library/Patron.js
@@ -1,8 +1,26 @@
 class Patron {
-   constructor(name, email, currentBook){
-    this.name = name;
-    this.email = email;
-    this.currentBook = null;
+   constructor(name, email, currentBook) {
+      this.name = name;
+      this.email = email;
+      this.currentBook = null;
+   }
+
+   checkOut(book) {
+      this.currentBook = book;
+      book.out = true;
+      book.patron = this;
+      //date +14 days
+      const newDueDate = new Date();
+      newDueDate.setDate(newDueDate.getDate() + 14);
+      book.dueDate = newDueDate;
+   }
+
+   returnBook(book) {
+      this.currentBook = null;
+      book.out = false;
+      book.patron = null;
+      book.dueDate = null;
+
    }
 
 }
